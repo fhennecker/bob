b621e87ba2ccbe117484e710c35843cc8a65e08d
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-01-01 13:13:11 +0100

    [fix] unpressing button after choosing food so it doesn't wrongly transition to other states in other FSMs

--- characteristics.scm
+++ characteristics.scm
@@ -102,10 +102,12 @@
         'button0-pressed?
         (lambda (measure)
           (define (choose-meal)
-            (context 'update-buttons)
-            (cond ((context 'button-pressed? 0) (measure 'update 50))
-                  ((context 'button-pressed? 1) (measure 'update 100))
-                  ((context 'button-pressed? 2) (measure 'update 300))
+            (context 'update-buttons) ; "unpress" the button which took us to this transition
+            ; wait for another button press, then "unpress" that button again so 
+            ; it doesn't transition to some other state in another fsm
+            (cond ((context 'button-pressed? 0) (measure 'update 50) (context 'update-buttons))
+                  ((context 'button-pressed? 1) (measure 'update 100) (context 'update-buttons))
+                  ((context 'button-pressed? 2) (measure 'update 300) (context 'update-buttons))
                   (else (choose-meal))))
           (choose-meal)
           (display-characteristic 1 (round (/ (measure 'value) 10)))
